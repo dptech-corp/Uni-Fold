e5007a4db69f83b4428484cf756e1d141bf5ffdb
ZiyaoLi <leeeezy@126.com>
2022-08-01 16:49:59 +0800

    log interval

--- train_multimer.sh
+++ train_multimer.sh
@@ -45,7 +45,7 @@ python -m torch.distributed.launch --nproc_per_node=$n_gpu --master_port $MASTER
        --optimizer adam --adam-betas '(0.9, 0.999)' --adam-eps 1e-6 --clip-norm 0.0  --per-sample-clip-norm 0.1 --allreduce-fp32-grad  \
        --lr-scheduler exponential_decay --lr $lr --warmup-updates $warmup_step --decay-ratio $decay_ratio --decay-steps $decay_step --stair-decay --batch-size 1 \
        --update-freq $update_freq --seed $seed  --tensorboard-logdir $2/tsb/ \
-       --max-update $total_step --max-epoch 1 --log-interval 1 --log-format simple \
+       --max-update $total_step --max-epoch 1 --log-interval 10 --log-format simple \
        --save-interval-updates 250 --validate-interval-updates 250 --keep-interval-updates 10 --no-epoch-checkpoints  \
        --save-dir $2 --tmp-save-dir $tmp_dir --required-batch-size-multiple 1 --bf16 --ema-decay 0.999 --data-buffer-size 32 --bf16-sr  --model-name $model_name
 
